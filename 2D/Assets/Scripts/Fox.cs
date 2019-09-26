05746ff219e2461144332edfeeacb77c641f2787
brite0316 <55218580+brite0316@users.noreply.github.com>
2019-09-26 12:06:28 +0800

    0926Ai

---
+++ 2D/Assets/Scripts/Fox.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("哈瞜");
+        Debug.Log("我誰");
+        Debug.Log(1.5f);
+        Debug.Log("我瘋子");
+    }
+}
