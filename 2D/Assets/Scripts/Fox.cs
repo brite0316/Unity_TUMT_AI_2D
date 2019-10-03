ef3ff4e22c072449ac57f480702d8bea5d2879b1
brite0316 <Giorge1234>
2019-10-03 12:02:05 +0800

    老鼠動態01003

--- 2D/Assets/Scripts/Fox.cs
+++ 2D/Assets/Scripts/Fox.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 
-public class Fox : MonoBehaviour
+public class Fox: MonoBehaviour
 {
-    private void Start()
-    {
-        Debug.Log("哈瞜");
-        Debug.Log("我誰");
-        Debug.Log(1.5f);
-        Debug.Log("我瘋子");
-    }
+    [Header("對話內容")]
+    public string start = "嗨! 幫我找水果";
+    public string not_complete = "我找到了";
+    public string complete = "感謝你的幫忙";
+
+    [Header("對話速度")]
+    public float speed = 1.5f;
+
+    [Header("任務相關")]
+    public bool missing_completer = true;
+    public string count_player = "0";
+    public string count_finish = "75";
+
+
+
+        
 }
